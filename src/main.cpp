469e326332f8d14f0ae514600b2f0818fdd9fdcc
Quang Duong <duongquang1@gmail.com>
2017-12-01 12:50:36 -0600

    Swap GLFW_TRUE to GL_TRUE so that it can run on lab machines

--- src/main.cpp
+++ src/main.cpp
@@ -39,7 +39,11 @@ static void error_callback(int error, const char* description) {
 
 static void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods) {
     if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
-        glfwSetWindowShouldClose(window, GLFW_TRUE);
+        glfwSetWindowShouldClose(window, GL_TRUE);
+    if (key == GLFW_KEY_1 && action == GLFW_RELEASE)
+        glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+    if (key == GLFW_KEY_2 && action == GLFW_RELEASE)
+        glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
 }
 
 int main(void) {
@@ -71,38 +75,43 @@ int main(void) {
     glGetError();
 
     // Generate the planet
-    float radius = 4.0f;
-    float camera_distance = 10.0f;
+    float radius = 2.0f;
+    float camera_distance = 6.0f;
     Icosphere planet;
-    for(int i = 0; i < 8; i++) {
+    for(int i = 0; i < 7; i++) {
         planet.subdivide();
     }
     for(int i = 0; i < planet.vertices.size(); i++) {
         auto vert = planet.vertices[i];
         auto v = vert.vertex;
         // Add 2 to move away from origin. Perlin does not handle negatives well
-        float noise = octave_perlin(v.x/4.0f + 2, v.y/4.0f + 2, v.z/4.0f + 2, 4, 3);
-        noise = noise * 0.1f - 0.05f;
-        planet.vertices[i] = {noise >= 0.0f ? (noise + radius) * v : v * radius,
-            noise >= 0.0f ?
-                glm::vec4(0.0f, 0.4f + 2 * noise, 0.0f, 1.0f) :
-                glm::vec4(0.0f, 0.0f, 0.4f + 2 * noise, 1.0f)};
+        float noise = (octave_perlin(v.x/4.0f + 2, v.y/4.0f + 2, v.z/4.0f + 2, 4, 3) + 0.25f * octave_perlin(v.x/4.0f + 2, v.y/4.0f + 2, v.z/4.0f + 2, 6, 3))/1.25f;
+        planet.vertices[i] = {noise >= 0.0f ? (noise * 0.6f - 0.3f + radius) * v : v * radius,
+            noise >= 0.7 ? 
+                glm::vec4(1.0f,1.0f,1.0f,1.0f) :
+            noise >= 0.6 ?
+                glm::vec4(1-noise, 1-noise, 1-noise, 1.0f) : (
+                noise >= 0.5f ?
+                    glm::vec4(0.0f, noise, 0.0f, 1.0f) : (
+                    noise >= 0.475f ?
+                        glm::vec4(noise, noise, 0.0f, 1.0f) :
+                        glm::vec4(0.0f, 0.0f, noise, 1.0f)))};
     }
 
     // Generate clouds
-    float cloud_radius = radius + 0.1f;
+    float cloud_radius = radius + 0.13f;
     Icosphere clouds;
-    for(int i = 0; i < 7; i++) {
+    for(int i = 0; i < 6; i++) {
         clouds.subdivide();
     }
     for(int i = 0; i < clouds.vertices.size(); i++) {
         auto vert = clouds.vertices[i];
         auto v = vert.vertex;
-        float noise = octave_perlin(v.x/3.f + 2, v.y/3.f + 2, v.z/3.f + 2, 4, 2);
+        float noise = octave_perlin(v.x/1.4f + 2, v.y/1.4f + 2, v.z/1.4f + 2, 4, 2);
         noise = noise * 0.6f - 0.3f;
-        clouds.vertices[i] = {noise >= 0.0f ? (noise + cloud_radius) * v : v * cloud_radius,
+        clouds.vertices[i] = {v * cloud_radius,
             noise >= 0.0f ?
-                glm::vec4(1.0f, 1.0f, 1.0f, 10 * noise) :
+                glm::vec4(1.0f, 1.0f, 1.0f, 5 * noise) :
                 glm::vec4(1.0f, 1.0f, 1.0f, 0.0f)};
     }
 
@@ -112,7 +121,7 @@ int main(void) {
     ratio = width / (float) height;
     glm::mat4 m = glm::rotate(glm::mat4(1.0f), float(glfwGetTime()), glm::vec3(0.0f, 1.0f, 0.0f));
     glm::mat4 v = glm::lookAt(glm::vec3(0.0f, 0.0f, -camera_distance), glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(0.0f, 1.0f, 0.0f));
-    glm::mat4 p = glm::perspective(45.0f, ratio, 0.0001f, 1000.0f);
+    glm::mat4 p = glm::perspective(45.0f, ratio, 0.01f, 100.0f);
     glm::mat4 mvp = p * v * m;
 
     VertexArrayObject planet_render({"vertex", "index"});
@@ -132,7 +141,7 @@ int main(void) {
             {"vPos", "vCol"}, 7, {0, 3}, {"MVP"},
             {[&mvp](GLint loc){glUniformMatrix4fv(loc, 1, GL_FALSE, glm::value_ptr(mvp));}}
             );
-
+    glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
     while (!glfwWindowShouldClose(window))
     {
         glfwGetFramebufferSize(window, &width, &height);
@@ -142,9 +151,9 @@ int main(void) {
         glEnable(GL_BLEND);
         glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-        glDepthFunc(GL_LESS);
+        glDepthFunc(GL_LEQUAL);
 
-        p = glm::perspective(45.0f, ratio, 0.0001f, 1000.0f);
+        p = glm::perspective(45.0f, ratio, 0.01f, 100.0f);
         m = glm::rotate(glm::mat4(1.0f), float(glfwGetTime()/4), glm::vec3(0.0f, 1.0f, 0.0f));
         mvp = p * v * m;
 
